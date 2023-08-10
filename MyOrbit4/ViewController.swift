519330927bec9ae16d4ec2d1c1123c517c3f3919
Doni Setiawan <donisetiawan.050@gmail.com>
2023-08-10 15:50:49 +0700

    add coordinator

--- MyOrbit4/ViewController.swift
+++ MyOrbit4/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var integratingFramework: IntegratingFramework?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let nvc = navigationController else { return }
+                integratingFramework = IntegratingFramework(navigationController: nvc)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didSelectButton(_ sender: UIButton){
+        print("clicked")
+        self.integratingFramework?.navigateTo(urlScheme: "mytelkomsel://orbit/content_details/0_xp5n50sz")
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'MyOrbit4' do
+  # Comment the next line if you don't want to use dynamic frameworks
+#  use_frameworks!
+  use_modular_headers!
+  
+  # Pods for MyOrbit4
+  pod 'OrbitCoordinator', :path => '../OrbitCoordinator'
+  
+  pod 'MyTelkomselSuperappFrameworkIntegration', :path => '../mytelkomsel-superapp-framework-integration-ios'
+  pod 'MyTelkomselSuperappFramework', :path => '../mytelkomsel-superapp-framework-ios'
+end
